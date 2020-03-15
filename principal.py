6c2d086917256f6baa474c8d4417c6e3eb2ed355
alexcham23 <tec-james.armira@outlook.com>
2020-03-14 19:58:33 -0600

    update1

--- principal.py
+++ principal.py
@@ -1,1 +1,17 @@
-print("hola")
+import msvcrt
+import menu
+def caratula():
+    print("*********************************************************")
+    print("*\t Lenguajes Formales de programación\t\t*")
+    print("*\t\t\tSeccion: A-\t\t\t*")
+    print("*\t\t\t 201602983\t\t\t*")
+    print("*********************************************************")
+
+def press_enter():
+    while True:
+        print("Presione enter para continuar ")
+        m= str(msvcrt.getch(),'utf -8')
+        if m == "\r":
+            menu.prueba()
+        break
+caratula()
