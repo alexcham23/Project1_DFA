d0dbbdf13f4751f78c85de9958f7c7b1efbe2cd2
alexcham23 <tec-james.armira@outlook.com>
2020-03-16 00:09:14 -0600

    update 1.0

---
+++ AFD.py
@@ -0,0 +1,45 @@
+nombre=estado=alfabeto=inicial=final=aceptacion=transiciones=""
+lista= list()
+def menuAFD():
+    op = 0
+    while op != 7:
+        print("=========================================================")
+        print("=\t\t 1.   Ingresar Estados\t\t\t=") 
+        print("=\t\t 2.   Ingresar Alfabeto\t\t\t=") 
+        print("=\t\t 3.   Estado Inicial\t\t\t=") 
+        print("=\t\t 4.   Estado de Aceptacion\t\t=")
+        print("=\t\t 5.   Transiciones \t\t\t=") 
+        print("=\t\t 6.   Ayuda\t\t\t=")
+        print("=\t\t 7.   Menu Principal\t\t\t=") 
+        print("=========================================================")
+        op = str(input("Elige una opcion:\n"))       
+
+        if op == '1' :
+            
+            break
+        elif op == '2' :
+           
+            break
+        elif op == '3' :
+           
+            break
+        elif op == '4' :
+           
+            break
+        elif op == '5' :
+           
+            break
+        elif op == '6' :
+            
+            break
+        elif op == '7' :
+            print("hola")
+            break
+def pedirnombre():
+    global nombre
+    nombre= str(input("Ingrese Un nombre para el AFD:\n "))
+    guardar=nombre,estado,alfabeto,inicial,final,aceptacion,transiciones
+    lista=(guardar)
+    menuAFD()
+
+menuAFD()
